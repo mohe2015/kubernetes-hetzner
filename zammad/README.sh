b082b29697d9a91ef691b7f130b451c4fb5aa499
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-08-22 18:08:57 +0200

    add note

--- zammad/README.sh
+++ zammad/README.sh
@@ -1,12 +1,15 @@
 
 # https://docs.zammad.org/en/latest/install/kubernetes.html
 
+# THIS APPLICATION IS SUPER SLOW
+
 # we now repeatedly experience node crashes (likely because of OOM)
 # is it maybe possible to set the memory limit of the node
 
+# https://github.com/zammad/zammad-helm
+
 helm repo add zammad https://zammad.github.io/zammad-helm
 helm upgrade --install zammad zammad/zammad --namespace=zammad --values zammad/values.yaml --create-namespace
-
 
 # kubectl get pods -n zammad -w
 
