70199794f6d19edc5863d9f230e66e8807e251b5
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-28 21:49:13 +0200

    dashboard

--- kubernetes-dashboard/README.sh
+++ kubernetes-dashboard/README.sh
@@ -5,4 +5,9 @@ helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
 
 helm upgrade kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --atomic --cleanup-on-fail --create-namespace --install --reset-values --values kubernetes-dashboard/values.yaml --namespace kubernetes-dashboard
 
-kubectl -n kubernetes-dashboard create token kubernetes-dashboard
+
+https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
+
+kubectl apply -f kubernetes-dashboard/user.yaml
+
+kubectl -n kubernetes-dashboard create token admin-user
