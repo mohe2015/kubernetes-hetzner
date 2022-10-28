45801abebfd4d92eb22123476407bd6348d682ae
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-29 00:09:44 +0200

    Fix mastodon

--- mastodon/README.sh
+++ mastodon/README.sh
@@ -7,6 +7,9 @@ kubectl create namespace mastodon
 
 kubectl config set-context --current --namespace=mastodon
 
-kubectl create secret generic mastodon-secret --from-literal=SECRET_KEY_BASE=$(openssl rand -base64 32) --from-literal=OTP_SECRET=$(openssl rand -base64 32) --from-literal=VAPID_PRIVATE_KEY=$(openssl rand -base64 32) --from-literal=VAPID_PUBLIC_KEY=$(openssl rand -base64 32) --from-literal=postgresql-password=$(openssl rand -base64 32) --from-literal=redis-password=$(openssl rand -base64 32)
+export POSTGRES_PASSWORD=$(openssl rand -base64 32)
+kubectl create secret generic mastodon-secret --from-literal=SECRET_KEY_BASE=$(openssl rand -base64 32) --from-literal=OTP_SECRET=$(openssl rand -base64 32) --from-literal=VAPID_PRIVATE_KEY=$(openssl rand -base64 32) --from-literal=VAPID_PUBLIC_KEY=$(openssl rand -base64 32) --from-literal=postgresql-password=$POSTGRES_PASSWORD --from-literal=postgres-password=$POSTGRES_PASSWORD --from-literal=redis-password=$(openssl rand -base64 32)
 
-helm install --debug --timeout 15m --namespace mastodon --create-namespace mastodon ./mastodon/mastodon/chart -f ./mastodon/values.yaml
+helm upgrade --install --debug --namespace mastodon --create-namespace mastodon ./mastodon/mastodon/chart -f ./mastodon/values.yaml
+
+https://mastodon.selfmade4u.de/
