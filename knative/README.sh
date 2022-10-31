ea0c36f08b8851448a552465afdc71fd8b8b40fe
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-31 20:54:35 +0100

    why

--- knative/README.sh
+++ knative/README.sh
@@ -4,35 +4,33 @@ docker run --rm -v "$HOME/.kube/config:/root/.kube/config" gcr.io/knative-releas
 
 # https://knative.dev/docs/install/yaml-install/
 
+# don't use operator as it does random reconciliation
+
 # TODO FIXME remove resouce limits
 
-#kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.8.0/serving-crds.yaml
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.8.0/serving-crds.yaml
 
-#kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.8.0/serving-core.yaml
-
-kubectl apply -f https://github.com/knative/operator/releases/download/knative-v1.8.0/operator.yaml
-
-kubectl config set-context --current --namespace=default
-
-kubectl get deployment knative-operator
+kubectl apply -f https://github.com/knative/serving/releases/download/knative-v1.8.0/serving-core.yaml
 
 # ADD DNS record for *.knative.selfmade4u.de
 
-kubectl logs -f deploy/knative-operator
+kubectl apply -f https://github.com/knative/net-istio/releases/download/knative-v1.8.0/net-istio.yaml
 
-kubectl apply -f knative/knative-serving.yaml
+kubectl get pods -n knative-serving
 
-kubectl get svc istio-ingressgateway -n istio-system
+kubectl patch configmap/config-domain \
+  --namespace knative-serving \
+  --type merge \
+  --patch '{"data":{"knative.selfmade4u.de":""}}'
 
-kubectl get deployment -n knative-serving
+kubectl -n knative-serving get -o yaml gateway knative-ingress-gateway
 
-kubectl get KnativeServing knative-serving -n knative-serving
+kubectl patch gateway/knative-ingress-gateway \
+  --namespace knative-serving \
+  --type merge \
+  --patch-file ./knative/knative-serving-tls.yaml
 
-kubectl apply -f knative/knative-eventing.yaml
-
-kubectl get deployment -n knative-eventing
-
-kubectl get KnativeEventing knative-eventing -n knative-eventing
+kubectl -n knative-serving get -o yaml gateway knative-ingress-gateway
 
 # install func by building from source
 
@@ -53,10 +51,3 @@ http://myfunc.default.knative.selfmade4u.de/
 
 
 ~/Documents/func/func delete myfunc
-
-
-kubectl config set-context --current --namespace=default
-
-istioctl proxy-config route service/knative-local-gateway.istio-system
-
-istioctl x describe service knative-local-gateway.istio-system
