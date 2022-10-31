92d6efe20d48a6769de0ebf08e72edaf0f9a329d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-10-31 14:40:07 +0100

    it listens but doesnt do shit

--- istio/README.sh
+++ istio/README.sh
@@ -9,33 +9,52 @@ kubectl config set-context --current --namespace=istio-system
 helm upgrade --install istio-base istio/base -n istio-system
 helm upgrade --install istiod istio/istiod -f istio/istiod-values.yaml -n istio-system --wait
 
-kubectl create namespace istio-ingress
-kubectl label namespace istio-ingress istio-injection=enabled
-kubectl config set-context --current --namespace=istio-ingress
+kubectl label namespace istio-system istio-injection=enabled
 
-helm template istio-ingress istio/gateway -f ./istio/ingress-values.yaml --namespace istio-ingress --post-renderer ./istio/kustomize.sh
-helm upgrade --install istio-ingress istio/gateway -f ./istio/ingress-values.yaml --namespace istio-ingress --wait --post-renderer ./istio/kustomize.sh
+#helm template istio-ingress istio/gateway -f ./istio/ingress-values.yaml --namespace istio-ingress --post-renderer ./istio/kustomize.sh
 
+# https://istio.io/latest/docs/setup/additional-setup/gateway/
+
+helm upgrade --install istio-ingressgateway istio/gateway -f ./istio/ingress-values.yaml --namespace istio-system --wait # --post-renderer ./istio/kustomize.sh
 
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.15/samples/httpbin/httpbin.yaml
 
 kubectl apply -f - <<EOF
-apiVersion: networking.k8s.io/v1
-kind: Ingress
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
 metadata:
-  annotations:
-    kubernetes.io/ingress.class: istio
-  name: ingress
+  name: httpbin-gateway
 spec:
-  rules:
-  - host: httpbin.selfmade4u.de
-    http:
-      paths:
-      - path: /status/*
-        pathType: Prefix
-        backend:
-          service:
-            name: httpbin
-            port:
-              number: 8000
+  selector:
+    istio: ingressgateway # use Istio default gateway implementation
+  servers:
+  - port:
+      number: 80
+      name: http
+      protocol: HTTP
+    hosts:
+    - "httpbin.selfmade4u.de"
+EOF
+
+kubectl apply -f - <<EOF
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: httpbin
+spec:
+  hosts:
+  - "httpbin.selfmade4u.de"
+  gateways:
+  - httpbin-gateway
+  http:
+  - match:
+    - uri:
+        prefix: /status
+    - uri:
+        prefix: /delay
+    route:
+    - destination:
+        port:
+          number: 8000
+        host: httpbin
 EOF
