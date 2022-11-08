ca8d0e60fa510889d758a8991c3d4484233cdb3c
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2022-11-08 21:45:25 +0100

    Fix mastodon

--- mastodon/README.sh
+++ mastodon/README.sh
@@ -15,14 +15,12 @@ kubectl label namespace mastodon istio-injection=enabled
 
 kubectl config set-context --current --namespace=mastodon
 
-kubectl create secret generic mastodon-secret --from-literal=SECRET_KEY_BASE=$(openssl rand -base64 32) --from-literal=OTP_SECRET=$(openssl rand -base64 32) --from-literal=VAPID_PRIVATE_KEY=$(openssl rand -base64 32) --from-literal=VAPID_PUBLIC_KEY=$(openssl rand -base64 32) --from-literal=postgresql-password=$(openssl rand -base64 32) --from-literal=postgres-password=$(openssl rand -base64 32) --from-literal=redis-password=$(openssl rand -base64 32) --from-literal=password=$(openssl rand -base64 32)
+kubectl create secret generic mastodon-secret --from-literal=SECRET_KEY_BASE=$(openssl rand -base64 32) --from-literal=OTP_SECRET=$(openssl rand -base64 32) --from-literal=VAPID_PRIVATE_KEY=$(openssl rand -base64 32) --from-literal=VAPID_PUBLIC_KEY=$(openssl rand -base64 32) --from-literal=redis-password=$(openssl rand -base64 32) --from-literal=password=$(openssl rand -base64 32) --from-literal=postgres-password=$(openssl rand -base64 32)
 
 helm upgrade --install --debug --namespace mastodon --create-namespace mastodon ./mastodon/mastodon/chart -f ./mastodon/values.yaml
-
-kubectl logs job/mastodon-create-admin
 
 kubectl get pods --watch
 
 https://mastodon.selfmade4u.de/
 
-kubectl exec -it deployment/mastodon-web -- sh
+kubectl exec -it deployment/mastodon-web -- tootctl accounts create moritz_hedtke --email=Moritz.Hedtke@t-online.de --confirmed --role=Owner
