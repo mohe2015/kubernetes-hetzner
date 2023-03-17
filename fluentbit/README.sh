a0c12d44b8e61569c3f2129ba68374d7174e5909
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-03-17 23:05:07 +0100

    Fluentbit

--- fluentbit/README.sh
+++ fluentbit/README.sh
@@ -1,16 +1,16 @@
 https://fluentbit.io/
 
-kubectl create namespace fluentd
-kubectl config set-context --current --namespace=fluentd
+kubectl create namespace fluent-bit
+kubectl config set-context --current --namespace=fluent-bit
 
 https://docs.fluentd.org/how-to-guides/free-alternative-to-splunk-by-fluentd
 
 helm repo add opensearch https://opensearch-project.github.io/helm-charts/
 helm repo update
 
-helm upgrade --install --namespace fluentd -f fluentbit/opensearch-values.yaml opensearch opensearch/opensearch
+helm upgrade --install --namespace fluent-bit -f fluentbit/opensearch-values.yaml opensearch opensearch/opensearch
 
-helm upgrade --install --namespace fluentd -f fluentbit/opensearch-values.yaml opensearch-dashboards opensearch/opensearch-dashboards
+helm upgrade --install --namespace fluent-bit -f fluentbit/opensearch-values.yaml opensearch-dashboards opensearch/opensearch-dashboards
 
 kubectl apply -f fluentbit/gateway.yaml
 
@@ -20,12 +20,19 @@ kubectl apply -f fluentbit/gateway.yaml
 
 helm repo add fluent https://fluent.github.io/helm-charts
 
-helm upgrade --install --namespace fluentd fluent-bit fluent/fluent-bit -f ./fluentbit/fluent-bit-values.yaml
+helm upgrade --install --namespace fluent-bit fluent-bit fluent/fluent-bit -f ./fluentbit/fluent-bit-values.yaml
 
-kubectl -n fluentd logs -f daemonset.apps/fluent-bit
+kubectl -n fluent-bit logs -f daemonset.apps/fluent-bit
 
-kubectl -n fluentd logs -f service/opensearch-cluster-master
+kubectl -n fluent-bit logs -f service/opensearch-cluster-master
 
+# Stack Management -> Index Patterns
 Index pattern: logstash-*
 Timestamp field: @timestamp
 
+# Discover
+
+# Anomaly Detection -> Detectors
+# logstash-*
+# Detector interval 10 minute
+# Window Delay 1 minute
