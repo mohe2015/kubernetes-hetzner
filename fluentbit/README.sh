346c1cb473ce42a9c13058bf04235fb0c3269536
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-03-17 21:44:22 +0100

    progress

--- fluentbit/README.sh
+++ fluentbit/README.sh
@@ -12,40 +12,20 @@ helm upgrade --install --namespace fluentd -f fluentbit/opensearch-values.yaml o
 
 helm upgrade --install --namespace fluentd -f fluentbit/opensearch-values.yaml opensearch-dashboards opensearch/opensearch-dashboards
 
+kubectl apply -f fluentbit/gateway.yaml
 
+# username: admin
+# password: admin
+# RESET PASSWORD
 
 helm repo add fluent https://fluent.github.io/helm-charts
 
-helm upgrade --install fluent-bit fluent/fluent-bit -f ./fluentd/fluent-bit-values.yaml --namespace fluentd
+helm upgrade --install --namespace fluentd fluent-bit fluent/fluent-bit -f ./fluentbit/fluent-bit-values.yaml
 
-kubectl auth can-i list pods --as=system:serviceaccount:fluentd:fluentbit
+kubectl -n fluentd logs -f daemonset.apps/fluent-bit
 
-export POD_NAME=$(kubectl get pods --namespace fluentd -l "app.kubernetes.io/name=fluent-bit,app.kubernetes.io/instance=fluent-bit" -o jsonpath="{.items[0].metadata.name}")
-kubectl --namespace fluentd port-forward $POD_NAME 2020:2020
-curl http://127.0.0.1:2020
-
-kubectl logs daemonset.apps/fluent-bit -f
-
-
-echo https://localhost:5601
-echo username elastic
-kubectl get secret elasticsearch-master-es-elastic-user -o=jsonpath='{.data.elastic}' | base64 --decode; echo
-kubectl port-forward service/elasticsearch-master-kb-http 5601
-# Kibana -> Stack Management -> Kibana -> Data Views -> Create data view
-
+kubectl -n fluentd logs -f service/opensearch-cluster-master
 
 Index pattern: logstash-*
 Timestamp field: @timestamp
 
-helm uninstall fluent-bit # don't simply delete the namespace
-
-# https://github.com/fluent/helm-charts/blob/main/charts/fluent-bit/templates/clusterrolebinding.yaml
-# kubectl get -o yaml ClusterRoleBinding fluent-bit
-kubectl auth can-i list pods -A --as=system:serviceaccount:fluentd:fluentbit
-
-kubectl apply -f fluentd/permissions.yaml
-
-kubectl delete pod/fluent-bit-8vwqt # because logs get rotated quickly
-kubectl logs daemonset.apps/fluent-bit | grep -B 15 -A 15 upstream
-
-https://github.com/istio/istio/issues/11130 reason why kubernetes connections fail at start
